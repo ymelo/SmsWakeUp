ff7a22bc1888d153f87a30d704b43a8b554e76a9
ymelo <yohann.melo@live.fr>
2016-08-27 10:37:14 -0400

    Added permission checking when starting fragment -- NEEDS TESTS
    Needs testing on device running 6+

--- mobile/src/main/java/com/ymelo/smswakeup/SettingsActivity.java
+++ mobile/src/main/java/com/ymelo/smswakeup/SettingsActivity.java
@@ -1,13 +1,18 @@
 package com.ymelo.smswakeup;
 
+import android.Manifest;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 
+import java.util.ArrayList;
 import java.util.Timer;
 
 
@@ -24,11 +29,50 @@ public class SettingsActivity extends PreferenceActivity{
 
     @TargetApi(Build.VERSION_CODES.HONEYCOMB)
     public static class SmsPreferenceFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener{
+        private static final int PERMISSION_REQUEST_WAKE_LOCK = 1;
+//        private static final int PERMISSION_REQUEST_READ_SMS = 10;
+//        private static final int PERMISSION_REQUEST_RECEIVE_SMS = 100;
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
 
+            ArrayList<String> permissionsRequested = new ArrayList<String>();
+            int wakePermission = ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WAKE_LOCK);
+
+            if(wakePermission != PackageManager.PERMISSION_GRANTED) {
+                permissionsRequested.add(Manifest.permission.WAKE_LOCK);
+            }
+//            if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED) {
+//                permissionsRequested.add(Manifest.permission.READ_SMS);
+//            }
+            if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+                permissionsRequested.add(Manifest.permission.RECEIVE_SMS);
+            }
+            if(permissionsRequested.size() > 0) {
+                String[] permissions = permissionsRequested.toArray(new String[permissionsRequested.size()]);
+                ActivityCompat.requestPermissions(getActivity(),
+                        permissions,
+                        PERMISSION_REQUEST_WAKE_LOCK);
+            }
+        }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if(requestCode == PERMISSION_REQUEST_WAKE_LOCK && grantResults.length > 0) {
+                boolean success = true;
+                for (int result: grantResults) {
+                    if(result != PackageManager.PERMISSION_GRANTED) {
+                        //Woot
+                    } else {
+                        //Permission refused...
+
+                    }
+                }
+            } else {
+                //failure?
+            }
         }
 
         @Override
@@ -53,6 +97,7 @@ public class SettingsActivity extends PreferenceActivity{
                     if(context != null) {
                         boolean activated = sharedPreferences.getBoolean(getString(R.string.pref_key_activated), false);
                         if(activated) {
+
                             SmsReceiver.enable(context);
                             if(BuildConfig.DEBUG && false) {
                                 Timer timer = new Timer();
